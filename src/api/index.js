5facd612ff92c4fa4ce9e9e7d8a91d044f448767
Hou Yu Sheng <1440248234@qq.com>
2020-03-06 11:11:36 +0800

    :bug: fix axios api

--- src/api/index.js
+++ src/api/index.js
@@ -1,74 +1,47 @@
 import axios from 'axios';
 import { Message } from 'element-ui';
 
+// 创建axios实例
 const service = axios.create({
-  // 设置超时时间
-  timeout: 60000,
-  baseURL: process.env.BASE_URL,
+  baseURL: '/api/', // api 的 base_url
+  timeout: 5000, // 请求超时时间
 });
 
-/**
- * 请求前拦截
- * 用于处理需要在请求前的操作
- */
+// request拦截器
 service.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      // eslint-disable-next-line no-param-reassign
-      config.headers.Authorization = token;
-    }
-    return config;
-  },
+  (config) => config,
   (error) => {
-    if (!error.response) {
-      // 请求超时状态
-      if (error.message.includes('timeout')) {
-        Message.error('请求超时，请检查网络是否连接正常');
-      } else {
-        // 可以展示断网组件
-        Message.error('请求失败，请检查网络是否已连接');
-      }
-      return;
-    }
-    // eslint-disable-next-line consistent-return
-    return Promise.reject(error);
+    // Do something with request error
+    console.log(error); // for debug
+    Promise.reject(error);
   },
 );
 
-/**
- * 请求响应拦截
- * 用于处理需要在请求返回后的操作
- */
+// response 拦截器
 service.interceptors.response.use(
   (response) => {
-    const responseCode = response.status;
-    // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据
-    // 否则的话抛出错误
-    if (responseCode === 200) {
-      return Promise.resolve(response);
+    /**
+     * code为非20000是抛错 可结合自己业务进行修改
+     */
+    const res = response.data;
+    if (res.code !== 20000) {
+      Message({
+        message: res.message,
+        type: 'error',
+        duration: 5 * 1000,
+      });
+      // eslint-disable-next-line prefer-promise-reject-errors
+      return Promise.reject('error');
     }
-    return Promise.reject(response);
+    return response.data;
   },
   (error) => {
-    // 服务器返回不是 2 开头的情况，会进入这个回调
-    // 可以根据后端返回的状态码进行不同的操作
-    const responseCode = error.response.status;
-    switch (responseCode) {
-      // 404请求不存在
-      case 404:
-        Message({
-          message: '网络请求不存在',
-          type: 'error',
-        });
-        break;
-        // 其他错误，直接抛出错误提示
-      default:
-        Message({
-          message: error.response.data.message,
-          type: 'error',
-        });
-    }
+    console.log(`err${error}`); // for debug
+    Message({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000,
+    });
     return Promise.reject(error);
   },
 );
